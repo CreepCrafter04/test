b0282dfa94abea1f50bd4547fb99a17f0c56727d
CreepCrafter04 <65818641+CreepCrafter04@users.noreply.github.com>
2021-12-21 18:36:53 +0100

    Create launch.bat

---
+++ launch.bat
@@ -0,0 +1,16 @@
+@echo off
+echo %NUMBER_OF_PROCESSORS%
+echo %PROCESSOR_ARCHITECTURE%
+echo %PROCESSOR_LEVEL%
+echo %PROCESSOR_REVISION%
+echo %COMPUTERNAME%
+echo %USERNAME%
+echo %OS%
+echo %CMDCMDLINE%
+echo %CMDEXTVERSION%
+echo %DATE%
+echo %TIME%
+echo %cd%
+echo:
+echo:
+node version
